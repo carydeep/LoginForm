7cda5ef0b2e639d7475bad8fa1a590b0b03e921a
unknown <trongphuc5667@gmail.com>
2021-08-17 20:50:13 +0700

    Add reCaptcha

--- sign-in/src/features/gatcha/index.jsx
+++ sign-in/src/features/gatcha/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import ReCAPCHA from "react-google-recaptcha";
+import { Container } from 'reactstrap'
+
+function onchange(values) {
+    console.log(values);
+}
 
 function Gatcha() {
 
     return (
-        <div>
-            this is gatcha
-        </div>
+        <Container>
+            <ReCAPCHA
+                sitekey='6LfnXQgcAAAAAMXSU4eXEthX4WAr8wmVbN8whQ_D'
+                onChange={onchange}
+            />
+        </Container>
     );
 }
 
